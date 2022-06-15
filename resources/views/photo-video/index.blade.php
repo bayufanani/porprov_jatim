e14c364bf8e2cfc1fd42187a0ec5f4e6dea8c6be
Bayu Fanani <fanani707@gmail.com>
2022-06-16 06:38:50 +0700

    fix routing and jsonFiles become attribute

--- resources/views/photo-video/index.blade.php
+++ resources/views/photo-video/index.blade.php
@@ -7,15 +7,15 @@
             <div class="row">
                 @foreach ($albums as $album)
                     <div class="col-12 col-md-3">
-                        <a href="{{ route('photo-video.show', ['id' => $album->id]) }}" style="color: black">
+                        <a href="{{ route('photo-video.show', ['album' => $album]) }}" style="color: black">
                             <div class="card">
                                 <div class="card-body">
                                     <h3><strong>{{ $album->judul }}</strong></h3>
-                                    <img src="{{ 'storage/' . $album->jsonFiles()[0] }}" alt="" class="img-fluid">
+                                    <img src="{{ 'storage/' . $album->jsonFiles[0] }}" alt="" class="img-fluid">
                                     <div class="row">
                                         @for ($i = 1; $i < 4; $i++)
-                                            @if (isset($album->jsonFiles()[$i]))
-                                                <img src="{{ 'storage/' . $album->jsonFiles()[$i] }}" alt=""
+                                            @if (isset($album->jsonFiles[$i]))
+                                                <img src="{{ 'storage/' . $album->jsonFiles[$i] }}" alt=""
                                                     class="img-fluid col-4">
                                             @endif
                                         @endfor
